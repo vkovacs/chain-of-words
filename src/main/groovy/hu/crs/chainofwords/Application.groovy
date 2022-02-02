0c11b396b492c4376b4005ebdcfac2bb09c20866
Viktor Kovacs <crusader8@gmail.com>
2022-02-02 17:33:02 +0100

    get path from parameter

--- src/main/groovy/hu/crs/chainofwords/Application.groovy
+++ src/main/groovy/hu/crs/chainofwords/Application.groovy
@@ -4,13 +4,13 @@ import hu.crs.chainofwords.word.WordService
 
 class Application {
     static void main(String[] args) {
-        String filename = args.size() > 0 ? args[0] : "words.txt"
+        String filename = args.size() > 0 ? args[0] : "src/main/resources/words.txt"
 
         WordService wordService = new WordService(load(filename))
         wordService.dictionary.each {println( it.key)}
     }
 
     static List<String> load(String filename) {
-        new File("src/main/resources/$filename").readLines()
+        new File(filename).readLines()
     }
 }
