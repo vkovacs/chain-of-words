d68bb4401891203d91000eb3f21363f4a44899be
Viktor Kovacs <crusader8@gmail.com>
2022-02-02 18:21:29 +0100

    answer

--- src/main/groovy/hu/crs/chainofwords/Application.groovy
+++ src/main/groovy/hu/crs/chainofwords/Application.groovy
@@ -7,7 +7,13 @@ class Application {
         String filename = args.size() > 0 ? args[0] : "src/main/resources/words.txt"
 
         WordService wordService = new WordService(load(filename))
-        wordService.dictionary.each {println( it.key)}
+        String word = ""
+        while (word != "bye") {
+            word = System.in.newReader().readLine()
+            def firstLetter = wordService.firstletter(word)
+            println wordService.next(firstLetter)
+        }
+        //wordService.dictionary.each {println( it.key)}
     }
 
     static List<String> load(String filename) {
