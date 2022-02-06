84670ec1d2174c7b196d69a19adfec83302280c0
Viktor Kovacs <crusader8@gmail.com>
2022-02-06 09:54:00 +0100

    add tests

--- src/test/groovy/hu/crs/chainofwords/word/WordServiceTest.groovy
+++ src/test/groovy/hu/crs/chainofwords/word/WordServiceTest.groovy
@@ -14,4 +14,22 @@ class WordServiceTest {
         assert underTest.dictionary['b'].size() == 1
         assert underTest.dictionary['d'].size() == 1
     }
+
+    @Test
+    void firstLetter() {
+        underTest = new WordService([])
+        assert underTest.firstletter("dzsidás") == "dzs"
+        assert underTest.firstletter("csirke") == "cs"
+        assert underTest.firstletter("óra") == "ó"
+
+    }
+
+    @Test
+    void lastLetter() {
+        underTest = new WordService([])
+        assert underTest.lastLetter("almudzs") == "dzs"
+        assert underTest.lastLetter("apacs") == "cs"
+        assert underTest.lastLetter("dió") == "ó"
+
+    }
 }
